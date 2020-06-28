a01a94734460df7677cbf1212f862f29b9d3f9f0
Jaskirat Grover <jaskirat23@hotmail.com>
2020-06-28 14:01:19 +1000

    Solutions to Conditional Expressions section

---
+++ ConditionalExpressions/Solution-1.scala
@@ -0,0 +1,14 @@
+// Solution-1.scala
+
+// 1. Set the values a and b to 1 and 5, respectively. Write a conditional
+// expression that checks to see if a is less than b. Print “a is less than
+// b” or “a is not less than b.”
+
+val a = 1
+val b = 5
+
+if(a < b) {
+    println("a is less than b")
+} else {
+    println("a is not less than b")
+}
---
+++ ConditionalExpressions/Solution-3.scala
@@ -0,0 +1,22 @@
+// Solution-3.scala
+
+// 3. Set the value c to 5. Modify the first exercise, above, to check if
+// a < c. Then, check if b < c (where ‘<’ is the less-than operator). Print
+// the results. 
+
+val a = 1
+val b = 5
+val c = 5
+
+
+if(a < c) {
+    println("a is less than c")
+} else {
+    println("a is not less than c")
+}
+
+if(b < c) {
+    println("b is less than c")
+} else {
+    println("b is not less than c")
+}
